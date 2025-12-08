7ccc940e8d773502e6eb96b186264f813699f79c
WR <yb3179174149@163.com>
2025-12-08 22:27:21 +0800

    gateway基本配置

---
+++ services/business-service/src/main/java/com/mindmooc/business/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.mindmooc.business.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RequestMapping("/api/business") //网关中已经添加过滤器处理这个冗余路径了
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String test() {
+        return "测试成功！";
+    }
+}
